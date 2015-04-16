9860bd8d34178b022daf80266a3c2e96fc8eb352
dmi3y <lapshukov@gmail.com>
2015-04-16 01:03:16 -0700

    arch test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-_MOCHA=node_modules/mocha/bin/mocha
+MOCHA=node_modules/mocha/bin/mocha
 
-_MOCHA --compilers coffee:coffee-script/register -R spec $(find test -name '*.test.coffee')
+$MOCHA --compilers coffee:coffee-script/register -R spec $(find test -name '*.test.coffee')
