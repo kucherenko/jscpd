01ee5af9e2edffeb43c2685878f07bc809600206
Wu Haotian <whtsky@gmail.com>
2018-11-27 23:05:38 +0800

    fix: support locally installed reporter and mode

--- src/utils/use.ts
+++ src/utils/use.ts
@@ -4,7 +4,7 @@ const detectInstalled = require('detect-installed');
 
 export function useReporter(name: string) {
   const reporterName = `jscpd-${name}-reporter`;
-  if (!detectInstalled.sync(reporterName)) {
+  if (!detectInstalled.sync(reporterName, { local: true })) {
     throw new Error(`Reporter "${reporterName}" does not found, please check that you have installed reporter package`);
   }
   return require(reporterName).default;
@@ -12,7 +12,7 @@ export function useReporter(name: string) {
 
 export function useMode(name: string): IMode {
   const modeName = `jscpd-${name}-mode`;
-  if (!detectInstalled.sync(modeName)) {
+  if (!detectInstalled.sync(modeName, { local: true })) {
     throw new Error(`Mode "${modeName}" does not found, please check that you have installed mode package`);
   }
   return require(modeName).default;
