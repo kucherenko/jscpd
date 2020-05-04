bf710f06efb92cb9f07ed996070d9eb3a1fefaee
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2020-05-04 18:23:06 +0300

    feat: 🎸 add redis store

--- typedoc.js
+++ typedoc.js
@@ -2,7 +2,12 @@ module.exports = {
   mode: 'modules',
   tsconfig: './packages/tsconfig.json',
   out: 'docs',
-  exclude: ['**/node_modules/**', '**/*.test.ts', '**/__tests__/**'],
+  exclude: [
+    '**/node_modules/**',
+    '**/*.test.ts',
+    '**/__tests__/**',
+    '**/publish-util.ts',
+  ],
   name: 'jscpd - copy/paste detector',
   excludePrivate: false,
   readme: 'README.md',
