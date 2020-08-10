5f41a13977802ef481c9cbc046992112104cddfe
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2020-08-10 15:48:24 +0300

    ci: 🎡 fix build

--- examples/example-jscpd.ts
+++
@@ -1,7 +0,0 @@
-import {IClone} from '@jscpd/core';
-import {jscpd} from 'jscpd';
-
-(async () => {
-  const clones: IClone[] = await jscpd(['', '', __dirname + '/../fixtures', '-m', 'weak', '--silent']);
-  console.log(clones);
-})();
